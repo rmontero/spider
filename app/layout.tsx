20d60804f9a7beb61d46b363b5e4f14603392872
Roberto Montero <r@rob.cr>
2026-03-14 12:38:50 -0600

    Add analytics
    Signed-off-by: Roberto Montero <r@rob.cr>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"`
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased font-sans">{children}</body>
+      <Analytics/>
     </html>
   );
 }
